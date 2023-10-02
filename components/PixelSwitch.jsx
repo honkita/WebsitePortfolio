96b4d2fca54dcaa318d550197cd4c92f59e7a59e
Elite Lu <elitelulww@gmail.com>
2023-10-02 00:01:49 -0400

    Fixed refresh bug

--- components/PixelSwitch.jsx
+++ components/PixelSwitch.jsx
@@ -17,68 +17,78 @@ export default function PixelSwitch() {
     return null;
   }
 
-  (function (_0x348535, _0x1e37c4) {
-    const _0x442dc6 = _0x2418,
-      _0x202825 = _0x348535();
-    while (!![]) {
-      try {
-        const _0x2dc904 =
-          parseInt(_0x442dc6(0x121)) / 0x1 +
-          (parseInt(_0x442dc6(0x124)) / 0x2) *
-            (parseInt(_0x442dc6(0x126)) / 0x3) +
-          parseInt(_0x442dc6(0x125)) / 0x4 +
-          (-parseInt(_0x442dc6(0x12a)) / 0x5) *
-            (-parseInt(_0x442dc6(0x12b)) / 0x6) +
-          parseInt(_0x442dc6(0x122)) / 0x7 +
-          -parseInt(_0x442dc6(0x127)) / 0x8 +
-          -parseInt(_0x442dc6(0x129)) / 0x9;
-        if (_0x2dc904 === _0x1e37c4) break;
-        else _0x202825["push"](_0x202825["shift"]());
-      } catch (_0x15e2b2) {
-        _0x202825["push"](_0x202825["shift"]());
-      }
-    }
-  })(_0x18ae, 0xaac45);
-  const toggleTheme = () => {
-    const _0x45516e = _0x2418;
-    resolvedTheme == _0x45516e(0x128)
-      ? (setTheme("light"), setPress(press + 0x1))
-      : (setTheme(_0x45516e(0x128)), setPress(press + 0x1)),
-      console["log"](press),
-      press >= 0x32 &&
-        (setPress(0x0), window[_0x45516e(0x123)]("../extra", _0x45516e(0x120)));
-  };
-  function _0x2418(_0x543e3c, _0x4148e5) {
-    const _0x18ae94 = _0x18ae();
+  function _0x41c9() {
+    const _0x3e4336 = [
+      "/extra",
+      "_self",
+      "6452330HNyRmT",
+      "dark",
+      "261aZJzVq",
+      "6luQIEy",
+      "259vomggk",
+      "1282942ApIfUO",
+      "14336290iwPPSb",
+      "147534oFFuVy",
+      "119712NBgFtF",
+      "./extra",
+      "244850hpvfpV",
+      "pathname",
+      "open",
+      "2793380yRtHrN",
+      "location",
+      "11mkPNUu",
+    ];
+    _0x41c9 = function () {
+      return _0x3e4336;
+    };
+    return _0x41c9();
+  }
+  function _0x4d98(_0x43c586, _0x30e34e) {
+    const _0x41c941 = _0x41c9();
     return (
-      (_0x2418 = function (_0x241833, _0x41d5fe) {
-        _0x241833 = _0x241833 - 0x120;
-        let _0x35b485 = _0x18ae94[_0x241833];
-        return _0x35b485;
+      (_0x4d98 = function (_0x4d980a, _0x31eb1a) {
+        _0x4d980a = _0x4d980a - 0x1b3;
+        let _0x1e31e3 = _0x41c941[_0x4d980a];
+        return _0x1e31e3;
       }),
-      _0x2418(_0x543e3c, _0x4148e5)
+      _0x4d98(_0x43c586, _0x30e34e)
     );
   }
-  function _0x18ae() {
-    const _0x66cb2f = [
-      "_self",
-      "987325GDpLxO",
-      "422499ClutgH",
-      "open",
-      "862OaePaR",
-      "4520192KYJipH",
-      "6879LQjhFC",
-      "4541552otbGNT",
-      "dark",
-      "20223324FupLth",
-      "535ACZzqp",
-      "19524LGRLJg",
-    ];
-    _0x18ae = function () {
-      return _0x66cb2f;
-    };
-    return _0x18ae();
-  }
+  (function (_0xbc72de, _0x1c3ff2) {
+    const _0x14bce5 = _0x4d98,
+      _0x534898 = _0xbc72de();
+    while (!![]) {
+      try {
+        const _0x5d0300 =
+          -parseInt(_0x14bce5(0x1b7)) / 0x1 +
+          (-parseInt(_0x14bce5(0x1c4)) / 0x2) *
+            (parseInt(_0x14bce5(0x1c2)) / 0x3) +
+          parseInt(_0x14bce5(0x1ba)) / 0x4 +
+          -parseInt(_0x14bce5(0x1bf)) / 0x5 +
+          (-parseInt(_0x14bce5(0x1b4)) / 0x6) *
+            (-parseInt(_0x14bce5(0x1c3)) / 0x7) +
+          (-parseInt(_0x14bce5(0x1b5)) / 0x8) *
+            (-parseInt(_0x14bce5(0x1c1)) / 0x9) +
+          (-parseInt(_0x14bce5(0x1b3)) / 0xa) *
+            (-parseInt(_0x14bce5(0x1bc)) / 0xb);
+        if (_0x5d0300 === _0x1c3ff2) break;
+        else _0x534898["push"](_0x534898["shift"]());
+      } catch (_0x496592) {
+        _0x534898["push"](_0x534898["shift"]());
+      }
+    }
+  })(_0x41c9, 0xa0839);
+  const toggleTheme = () => {
+    const _0x4c627a = _0x4d98;
+    resolvedTheme == _0x4c627a(0x1c0)
+      ? setTheme("light")
+      : setTheme(_0x4c627a(0x1c0)),
+      window[_0x4c627a(0x1bb)][_0x4c627a(0x1b8)] != _0x4c627a(0x1bd) &&
+        setPress(press + 0x1),
+      press == 0x32 &&
+        (window[_0x4c627a(0x1b9)](_0x4c627a(0x1b6), _0x4c627a(0x1be)),
+        setPress(0x0));
+  };
 
   return (
     <ThemeProvider>
