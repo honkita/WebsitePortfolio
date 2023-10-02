a448a6f77757c512a3c8da956d135cc3939e9906
Elite Lu <elitelulww@gmail.com>
2023-10-02 12:40:20 -0400

    LinkedIn button fix 2

--- components/PixelButton.jsx
+++ components/PixelButton.jsx
@@ -49,8 +49,8 @@ export default function PixelButton(props) {
       target: "_blank",
     },
     LinkedIn: {
-      lightTheme: "./images/Buttons/PixelLinkedIn.png",
-      darkTheme: "./images/Buttons/PixelLinkedInDark.png",
+      lightTheme: "./images/Buttons/PixelLIn.png",
+      darkTheme: "./images/Buttons/PixelLInDark.png",
       target: "_blank",
     },
   };
